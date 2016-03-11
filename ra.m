ecbf3c44d2af05b5ec7c040d52f458a11a3de834
Oliver Schaefer <Oliver.Schaefer@fu-berlin.de>
2016-03-12 00:40:44 +0100

    Vervollständigung der RA-Schnittstelle

--- ra.m
+++ ra.m
@@ -1,51 +1,107 @@
 abstype ra
 with
-  ||         name      zeichenkette
-  allOf   :: [char] -> [char] -> ra
-  oneOf   :: [char] -> [char] -> ra
+  ||         name      zeichen
+  all     :: [char] -> [char] -> ra
+  choice  :: [char] -> [char] -> ra
+  part    :: [char] -> [char] -> ra
 
   || Kombinierer von RAs
-  raOr    :: ra -> ra -> ra
-  || RA:   a -> b -> a|b
+  raOr    :: [ra] -> ra
+  || RA:   [r1,r1,r3,...] -> r1|r2|r3|...
   || Vor.: keine
-  || Erg.: In 'raOr a b' ist ein RA geliefert, der auf all die Wörter w passt,
-  ||       auf die a ODER b passt.
+  || Erg.: In 'raOr [r1,r2,r3,...]' ist ein RA geliefert, der auf alle Wörter
+  ||       w passt, auf die r1 ODER r2 ODER r3 ODER ... passt.
+  ||       Der Bezeichner des RA ist aus den Bezeichnern von r1,r2,r3,... zu-
+  ||       sammengesetzt mit eingeschobenen '|'.
 
-  raAnd   :: ra -> ra -> ra
-  || RA:   a -> b -> ab
+  raAnd   :: [ra] -> ra
+  || RA:   [r1,r2,r3,...] -> r1r2r3...
   || Vor.: keine
-  || Erg.: In 'raAnd a b' ist ein  RA  geliefert, der  auf  all die Wörter vw
-  ||       passt, für die auf v UND b auf w passen.
+  || Erg.: In 'raAnd [r1,r2,r3,...]' ist  ein  RA geliefert, der auf  all die
+  ||       Wörter w1w2w3 passt, für die r1 auf w1 UND r2 auf w2 UND r3 auf w3
+  ||       UND ... passen.
+  ||       Der Bezeichner des RA ist aus den Bezeichnern von r1,r2,r3,... zu-
+  ||       sammengesetzt (konkateniert).
 
   raStar  :: ra -> ra
-  || RA:   a -> a*
+  || RA:   r -> r*
   || Vor.: keine
-  || Erg.: In 'raStar a' ist ein RA geliefert, der  auf beliebige, auch kein-
-  ||       malige Wiederholungen von Wörtern passt, auf die a passt.
+  || Erg.: In 'raStar r' ist ein RA geliefert, der  auf beliebige, auch kein-
+  ||       malige Wiederholungen von Wörtern passt, auf die r passt.
+  ||       Besteht der Bezeichner <r> von r  nur aus  einem  Zeichen, ist der
+  ||       zeichner von 'raStar r' <r>*, ansonsten (<r>)*.
 
   raPlus  :: ra -> ra
-  || RA:   a -> a+
+  || RA:   r -> r+
   || Vor.: keine
-  || Erg.: In 'raStar a' ist ein RA geliefert, der  auf  beliebige, aber min-
+  || Erg.: In 'raStar r' ist ein RA geliefert, der  auf  beliebige, aber min-
   ||       destens einmalige Wiederholungen von Wörtern passt, auf die der RA
-  ||       a passt.
+  ||       r passt.
+  ||       Besteht der Bezeichner <r> von r  nur aus  einem  Zeichen, ist der
+  ||       zeichner von 'raPlus r' <r>+, ansonsten (<r>)+.
 
   ra0or1  :: ra -> ra
-  || RA:   a -> a?
+  || RA:   r -> r?
   || Vor.: keine
-  || Erg.: In 'raStar a' ist ein  RA geliefert, der  auf kein- oder einmalige
-  ||       Wiederholung von Wörtern passt, auf die a passt.
+  || Erg.: In 'raStar r' ist ein  RA geliefert, der  auf kein- oder einmalige
+  ||       Wiederholung von Wörtern passt, auf die r passt.
+  ||       Besteht der Bezeichner <r> von r  nur aus  einem  Zeichen, ist der
+  ||       zeichner von 'ra0or1 r' <r>?, ansonsten (<r>)?.
+
+  raMaxN  :: ra -> num -> ra
+  || RA:   r -> n -> r{,n}
+  || Vor.: keine
+  || Erg.: In 'raMaxN r n' ist ein RA geliefert, der auf kein- bis maximal n-
+  ||       malige Wiederholung von Wörtern passt, auf die r passt.
+  ||       Besteht der Bezeichner <r> von r  nur aus  einem  Zeichen, ist der
+  ||       zeichner von 'raMaxN r' <r>{,n}, ansonsten (<r>){,n}.
+
+  raMinN  :: ra -> num -> ra
+  || RA:   r -> n -> r{n,}
+  || Vor.: keine
+  || Erg.: In 'raMaxN r n' ist ein RA  geliefert, der auf mindestens n-malige
+  ||       Wiederholung von Wörtern passt, auf die r passt.
+  ||       Besteht der Bezeichner <r> von r  nur aus  einem  Zeichen, ist der
+  ||       zeichner von 'raMaxN r' <r>{n,}, ansonsten (<r>){n,}.
+
+  raN      :: ra -> num -> ra
+  || RA:   r -> n -> r{n}
+  || Vor.: keine
+  || Erg.: In 'raMaxN r n' ist ein RA  geliefert, der auf genau n-malige Wie-
+  ||       derholung von Wörtern passt, auf die r passt.
+  ||       Besteht der Bezeichner <r> von r  nur aus  einem  Zeichen, ist der
+  ||       zeichner von 'raMaxN r' <r>{n}, ansonsten (<r>){n}.
+
+  raMN     :: ra -> num -> num -> ra
+  || RA:   r -> m -> n -> r{m,n}
+  || Vor.: keine
+  || Erg.: In 'raMaxN r n' ist ein  RA  geliefert,  der auf mindestens n- und
+  ||       höchstens m-malige Wiederholung von Wörtern passt, auf die r passt.
+  ||       Besteht der Bezeichner <r> von r  nur aus  einem  Zeichen, ist der
+  ||       zeichner von 'raMaxN r' <r>{m,n}, ansonsten (<r>){m,n}.
 
   matches :: ra -> [char] -> bool
   showra  :: ra -> [char]
 
   || spezielle RAs, die in RA-Implementierungen typischerweise verwendet wer-
   || den (siehe z.B. Skript, Seite 3-3).
+
   alpha_  :: ra
+  ||      RA, der auf ein beliebigen Buchstaben matcht.
+
   digit_  :: ra
+  ||      RA, der auf eine beliebige Dezimalziffer matcht.
+
   upper_  :: ra
+  ||      RA, der auf einen beliebigen Großbuchstaben matcht (ohne Umlaute).
+
   lower_  :: ra
+  ||      RA, der auf einen beliebigen Kleinbuchstaben matcht (ohne Umlaute).
+
   alnum_  :: ra
+  ||      RA, der auf einen beliebigen Buchstaben oder Dezimalziffer matcht.
+
   punct_  :: ra
+  ||      RA, der auf ein Zeichen aus ".,:;?!" matcht.
 
 %insert "raimpl.m"
--- raimpl.m
+++ raimpl.m
@@ -7,21 +7,65 @@
 
 ra == (automat,[char])
 
-alpha_ = oneOf "[:alpha:]"  letters
-digit_ = oneOf "[:digits:]" digits
-alnum_ = oneOf "[:alnum:]"  (digits ++ letters)
-punct_ = oneOf "[:punct:]"  ".,:;!?"
-upper_ = oneOf "[:upper:]"  capitals
-lower_ = oneOf "[:lower:]"  small
+alpha_ = choice "[:alpha:]"  letters
+digit_ = choice "[:digits:]" digits
+alnum_ = choice "[:alnum:]"  (digits ++ letters)
+punct_ = choice "[:punct:]"  ".,:;!?"
+upper_ = choice "[:upper:]"  capitals
+lower_ = choice "[:lower:]"  small
+
+raMaxN r n
+  = (fst r', "(" ++ snd r ++ "){," ++ shownum n ++ "}"), if #(snd r) > 1
+  = (fst r',        snd r ++  "{," ++ shownum n ++ "}"), otherwise
+    where
+    r' = raOr ([ra0or1 r] ++ [raAnd (rep k r)|k<-[2..n]])
+
+|| TODO, terminiert nicht
+raMinN r n
+  = (fst r', "(" ++ snd r ++ "){" ++ shownum n ++ ",}"), if #(snd r) > 1
+  = (fst r',        snd r ++  "{" ++ shownum n ++ ",}"), otherwise
+    where
+    r' = raOr [raAnd (rep k r)|k<-[n..]]
+
+raN r n
+  = (fst r', "(" ++ snd r ++ "){" ++ shownum n ++ "}"), if #(snd r) > 1
+  = (fst r',        snd r ++  "{" ++ shownum n ++ "}"), otherwise
+    where
+    r' = raAnd (rep n r)
+
+raMN r m n
+  = (fst r', "(" ++ snd r ++ "){" ++ shownum m ++ "," ++ shownum n ++ "}"), if #(snd r) > 1
+  = (fst r',        snd r ++  "{" ++ shownum m ++ "," ++ shownum n ++ "}"), otherwise
+    where
+    r' = raOr [raAnd (rep k r)|k<-[m..n]]
 
 raPlus (a,n)
-  = (fst (raAnd (a,"") (raStar (a,n))), "(" ++ n ++ ")+")
+  = (fst (raAnd [(a,""),raStar (a,n)]), "(" ++ n ++ ")+"), if #n < 2
+  = (fst (raAnd [(a,""),raStar (a,n)]),        n ++  "+"), otherwise
 
-||ra0or1 (a,n)
-||  = (ra0or1' a, "(" ++ n ++ ")?")
+ra0or1 (a,n)
+  = (ra0or1' a, "(" ++ n ++ ")?"), if #n > 1
+  = (ra0or1' a,        n ++  "?"), otherwise
+    where
+    ra0or1' a
+      = neaE qs' zs' ts'
+        where
+        qs' = map (shiftQ 1 . chgStart . chgAccept) qsA ++ [(n,Accept)]
+        qsA = states a
+        n   = max (map fst qsA) + 2
+        shiftQ x (q,t) = (q+x,t)
+        chgAccept (n,Accept) = (n,Inner)
+        chgAccept (n,t)      = (n,t)
+        chgStart  (0,Start)  = (0,Inner)
+        chgStart  (n,t)      = (n,t)
+        zs' = sigma a
+        tsA = transitions a
+        ts' = map (shiftT 1) tsA ++ [(0,"",1),(0,"",n)] ++ [(k+1,"",n)|(k,Accept)<-qsA]
+        shiftT x (qi,z,qj) = (qi+x,z,qj+x)
 
 raStar (a,n)
-  = (raStar' a, "(" ++ n ++ ")*")
+  = (raStar' a, "(" ++ n ++ ")*"), if #n > 1
+  = (raStar' a,        n ++  "*"), otherwise
     where
     raStar' a
       = neaE qs' zs' ts'
@@ -39,8 +83,8 @@ raStar (a,n)
         ts' = map (shiftT 1) tsA ++ [(0,"",1),(0,"",n),(n,"",0)] ++ [(k+1,"",n)|(k,Accept)<-qsA]
         shiftT x (qi,z,qj) = (qi+x,z,qj+x)
 
-raOr (a1,n1) (a2,n2)
-  = (raOr' a1 a2, "(" ++ n1 ++ "|" ++ n2 ++ ")")
+raOr rs
+  = (foldr1 raOr' (map fst rs), "(" ++ (concat . (intersperse "|") . (map snd)) rs ++ ")")
     where
     raOr' a b
       = neaE qs' zs' ts'
@@ -58,8 +102,8 @@ raOr (a1,n1) (a2,n2)
         ts' = map (shiftT 1) tsA ++ map (shiftT n) tsB ++ [(0,"",1),(0,"",n)]
         shiftT x (qi,z,qj) = (qi+x,z,qj+x)
 
-raAnd (a1,n1) (a2,n2)
-  = (raAnd' a1 a2, "(" ++ n1 ++ n2 ++ ")")
+raAnd rs
+  = (foldr1 raAnd' (map fst rs), (concat . (intersperse "") . (map snd)) rs)
     where
     raAnd' a b
       = neaE qs' zs' ts'
@@ -81,9 +125,16 @@ raAnd (a1,n1) (a2,n2)
 
 matches = accepts . fst
 
-|| 'allOf xs' liefert einen RA, der auf die durch xs repräsentierte Zeichenkette
-|| matcht
-allOf name wort
+all name wort
+  = (sucher wort, name)
+    where
+    sucher b = nea qs zs ts
+               where 
+               qs = [(i,Inner)|i<-[1..#b-1]] ++ [(#b,Accept)]
+               zs = ascii
+               ts = [(i,[b!i],i+1)|i<-[0..#b-1]]
+
+part name wort
   = (sucher wort, name)
     where
     sucher b = nea qs zs ts
@@ -92,12 +143,15 @@ allOf name wort
                zs = ascii
                ts = [(0,zs,0),(#b,zs,#b)] ++ [(i,[b!i],i+1)|i<-[0..#b-1]]
 
-|| 'oneOf xs' liefert einen RA, der auf ein beliebiges Zeichen aus xs matcht.
-oneOf name wort
+choice name wort
   = (nea qs zs ts, name)
     where
     qs = [(0,Start)] ++ [(i,Accept)|i<-[1..#wort]]
     zs = ascii
     ts = [(0,[wort!i],i+1)|i<-[0..#wort-1]]
 
-showra (a,name) = name
+showra = snd
+
+|| ------------------------ Hilfsfunktionen -----------------------------
+intersperse :: * -> [*] -> [*]
+intersperse x xs = (tl . concat) [[x,y]|y<-xs]
